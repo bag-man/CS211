292cade93430d858c8a8011a8269f37b28cf05a4
bag-man <garland.owen@gmail.com>
2014-11-20 12:08:03 +0000

    Removed folder structure

--- uk/ac/aber/dcs/owg1/cs21120/assignment1/QueueEmptyException.java
+++
@@ -1,6 +0,0 @@
-package uk.ac.aber.dcs.owg1.cs21120.assignment1;
-public class QueueEmptyException extends RuntimeException {
-  public QueueEmptyException() {
-    super("Attempt to access empty Queue");
-  }
-} 
